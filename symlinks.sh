7dd82da0745ed11175f763d0f486e9f37fdfe570
Vryali <vryali@gmail.com>
2023-04-06 01:25:50 -0600

    Modified plugins

--- symlinks.sh
+++ symlinks.sh
@@ -3,7 +3,8 @@ FFXIV_DIR="${HOME}/.xlcore/pluginConfigs"
 SRC_DIR="${HOME}/Source/personal/ffxiv-plugins"
 
 # Just define them staticly for now
-plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json"
+plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json BDTHPlugin.json BigPlayerDebuffs.json PartyIcons.json SimpleTweaksPlugin.json SoundFilter.json XIVCombo.json RezPls.json"
 for file in $plugins; do
     ln -sf ${SRC_DIR}/${file} ${FFXIV_DIR}/${file}
 done
+
