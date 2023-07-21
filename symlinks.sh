6ac0b4889d14c3c94b4471be445ce76f8691ca8b
Vryali <vryali@gmail.com>
2023-07-21 17:02:13 -0600

    Added deathrecap

--- symlinks.sh
+++ symlinks.sh
@@ -3,7 +3,7 @@ FFXIV_DIR="${HOME}/.xlcore/pluginConfigs"
 SRC_DIR="${HOME}/Source/personal/ffxiv-plugins"
 
 # Just define them staticly for now
-plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json BDTHPlugin.json BigPlayerDebuffs.json PartyIcons.json SimpleTweaksPlugin.json SoundFilter.json XIVCombo.json RezPls.json NeatNoter.json"
+plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json BDTHPlugin.json BigPlayerDebuffs.json PartyIcons.json SimpleTweaksPlugin.json SoundFilter.json XIVCombo.json RezPls.json NeatNoter.json DeathRecap.json"
 for file in $plugins; do
     if [ ! -h "${FFXIV_DIR}/${file}" ]; then
         # It's not a symlink, move it here and recreate the symlink
