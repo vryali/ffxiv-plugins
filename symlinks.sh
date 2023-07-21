45db1f39f140b05fefaec1d4385898ad491e4723
Vryali <vryali@gmail.com>
2023-07-21 17:00:35 -0600

    Updated symlink/copy method

--- symlinks.sh
+++ symlinks.sh
@@ -3,8 +3,14 @@ FFXIV_DIR="${HOME}/.xlcore/pluginConfigs"
 SRC_DIR="${HOME}/Source/personal/ffxiv-plugins"
 
 # Just define them staticly for now
-plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json BDTHPlugin.json BigPlayerDebuffs.json PartyIcons.json SimpleTweaksPlugin.json SoundFilter.json XIVCombo.json RezPls.json"
+plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json BDTHPlugin.json BigPlayerDebuffs.json PartyIcons.json SimpleTweaksPlugin.json SoundFilter.json XIVCombo.json RezPls.json NeatNoter.json"
 for file in $plugins; do
-    ln -sf ${SRC_DIR}/${file} ${FFXIV_DIR}/${file}
+    if [ ! -h "${FFXIV_DIR}/${file}" ]; then
+        # It's not a symlink, move it here and recreate the symlink
+        mv ${FFXIV_DIR}/${file} ${SRC_DIR}/${file}
+        ln -sf ${SRC_DIR}/${file} ${FFXIV_DIR}/${file}
+        echo "... Copied $file to repo"
+    else
+        echo "${file}: is link."
+    fi
 done
-
