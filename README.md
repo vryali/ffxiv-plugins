70cce2bf44eae5fd3dc82dda8053ce2ad555e88b
Vryali <vryali@gmail.com>
2023-08-09 13:11:38 -0600

    Normal update

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # ffxiv-plugins
-#
-
-## pwaymark
-It's just json, but if we do it this way we can ensure they stay up-to-date and the same between Windows/Linux
-/home/vry/.xlcore/pluginConfigs
+It's just json, but if we do it this way we can ensure they stay up-to-date across systems
--- symlinks.sh
+++ symlinks.sh
@@ -2,15 +2,17 @@
 FFXIV_DIR="${HOME}/.xlcore/pluginConfigs"
 SRC_DIR="${HOME}/Source/personal/ffxiv-plugins"
 
-# Just define them staticly for now
-plugins="PatMe.json TeleporterPlugin.json WaymarkPresetPlugin.json BDTHPlugin.json BigPlayerDebuffs.json PartyIcons.json SimpleTweaksPlugin.json SoundFilter.json XIVCombo.json RezPls.json NeatNoter.json DeathRecap.json"
-for file in $plugins; do
+# All the json files here should be plugins, they get overwritten on updates
+# and nothing we seem to do will change that, so whatever, just overwrite them
+# here when it happens.
+plugins="$(ls *.json)"
+for file in $plugins; do 
     if [ ! -h "${FFXIV_DIR}/${file}" ]; then
         # It's not a symlink, move it here and recreate the symlink
         mv ${FFXIV_DIR}/${file} ${SRC_DIR}/${file}
         ln -sf ${SRC_DIR}/${file} ${FFXIV_DIR}/${file}
         echo "... Copied $file to repo"
     else
-        echo "${file}: is link."
+        echo "${file}: already a symlink link."
     fi
 done
