d24fd9f72a38c788805d320ee0fe9fc25b9fd170
Vryali <vryali@gmail.com>
2024-02-21 15:37:37 -0700

    Added basic subfolders and functionality

--- symlinks.sh
+++ symlinks.sh
@@ -6,6 +6,7 @@ SRC_DIR="${HOME}/Source/personal/ffxiv-plugins"
 # and nothing we seem to do will change that, so whatever, just overwrite them
 # here when it happens.
 plugins="$(ls *.json)"
+
 for file in $plugins; do 
     if [ ! -h "${FFXIV_DIR}/${file}" ]; then
         # It's not a symlink, move it here and recreate the symlink
@@ -13,6 +14,29 @@ for file in $plugins; do
         ln -sf ${SRC_DIR}/${file} ${FFXIV_DIR}/${file}
         echo "... Copied $file to repo"
     else
-        echo "${file}: already a symlink link."
+        echo "${file}: already a symlink."
     fi
 done
+
+# MapPartyAssist is nested, get those.
+MAP_DIR="${FFXIV_DIR}/MapPartyAssist"
+
+for file in `ls ${MAP_DIR}`; do
+    if [ ! -h "${MAP_DIR}/${file}" ]; then
+        # It's not a symlink, move it here and recreate the symlink
+        mv ${MAP_DIR}/${file} ${SRC_DIR}/MapPartyAssist/${file}
+        ln -sf ${SRC_DIR}/MapPartyAssist/${file} ${MAP_DIR}/${file}
+        echo "... Copied MapPartyAssist/$file to repo"
+    else
+        echo "MapPartyAssist/${file}: already a symlink."
+    fi
+done
+
+# Noted is nexted, just one file
+if [ ! -h "${FFXIV_DIR}/NOTED/Settings.json" ]; then
+    mv ${FFXIV_DIR}/NOTED/Settings.json ${SRC_DIR}/NOTED/Settings.json
+    ln -sf ${SRC_DIR}/NOTED/Settings.json ${FFXIV_DIR}/NOTED/Settings.json
+    echo "... Copied NOTED/Settings.json to repo"
+else
+    echo "NOTED/Settings.json: already a symlink."
+fi
